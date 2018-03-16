83375230c6809b8e34ca8686be8c038e44686637
Eric Nordstrom <Eric Nordstrom>
2018-03-16 14:08:15 -0500

    made sentence_tokenize.py more user-friendly

--- Sentence Tokenization/sentence_tokenize.py
+++ Sentence Tokenization/sentence_tokenize.py
@@ -24,57 +24,26 @@ Python 3.6.0
 
 
 import logging
+from string import punctuation
 logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 
-def _ispunc(TOK):
-    '''whether a token consists only of punctuation characters'''
-    return all([char in punc for char in TOK])
-
-
-def join_tokens(INTEXT):
-    '''Combine first column of .tsv contents into a corpus for later sentence tokenization. Also returns input contents split into lines.'''
-    global punc
-    from string import punctuation as punc
-    lines = INTEXT.splitlines()
-    corptext = ''
-    ncols = 0
-    for line in lines:
-        items = line.split('\t')
-        tok = items[0]
-        if len(items) > ncols:
-            ncols = len(items)
-        if not(_ispunc(tok)):
-            corptext += ' '
-        corptext += tok
-    del punc
-    corptext = corptext.lstrip(' ')
-    return corptext, lines, ncols
-
-
-def _disp_extns(extns, conj):
-    '''display a list of accepted file extensions in verbose form'''
-    disp = '.' + extns[-1]
-    L = len(extns)
-    if L > 1:
-        disp = ' ' + conj + ' ' + disp
-        if L > 2:
-            disp = ',' + disp
-        disp = '.' + ', .'.join(extns[:-1]) + disp
-    return disp
-
-
-def _ap_parser(accepted_file_types):
+def _ap_parser():
     '''set up and return the argparse parser'''
     import argparse
-    parser = argparse.ArgumentParser(description="For .txt input, writes 2 .txt files: (1) sentence-tokenized corpus text and (2) indices at which each sentence starts. For .tsv input, writes an output .tsv file with an additional column indicating the first token of each sentence.")
+    parser = argparse.ArgumentParser(description="Outputs a new TSV file with an additional column indicating the last token of each sentence. Input can be tokenized or untokenized at the word level.")
     parser.add_argument(
-        'text',
-        help="corpus file ({})".format(_disp_extns(accepted_file_types, 'or'))
+        'input_file',
+        help="corpus file"
     )
     parser.add_argument(
-        'language',
-        help="desired language of Punkt tokenizer model"
+        'tokenizer',
+        help="the desired sentence tokenizer *.pickle file"
+    )
+    parser.add_argument(
+        '-wt', '--word_tokenized',
+        action='store_true',
+        help="specify if the input is already tokenized at the word level"
     )
     parser.add_argument(
         '-e', '--encoding',
@@ -82,12 +51,19 @@ def _ap_parser(accepted_file_types):
         help="file encoding (default=UTF8)"
     )
     parser.add_argument(
-        '-tf', '--tokenizer_folder',
-        help="folder containing the desired Punkt tokenizer"
-    )
-    parser.add_argument(
         '-of', '--output_folder',
         help="folder to write output files"
+    )
+    parser.add_argument(
+        '-tc', '--token_column',
+        # no default; this way, can check if user specified
+        type=int,
+        help="index of column containing word-level tokens (default: 0)"
+    )
+    parser.add_argument(
+        '-d', '--delimiter',
+        # no default; this way, can check if user specified
+        help="delimter between columns in input file (default: Tab)"
     )
     parser.add_argument(
         '-l', '--log_level',
@@ -96,104 +72,116 @@ def _ap_parser(accepted_file_types):
     return parser
 
 
-# def _interpret_tokenizers_folder(user_input):
-#     '''interpret the user input for the location of the folder containing the `tokenizers` subfolder'''
-#     folders = user_input.split('\\')
-#     if folders[-1].lower() == 'punkt':
-#         if folders[-2].lower() == 'tokenizers':
-#             n = 2
-#         else:
-#             n = 1
-#         folders = folders[:-n]
-#     elif folders[-1].lower() == 'tokenizers':
-#         folders = folders[:-1]
-#     return '\\'.join(folders)
+def wordtok(sent):
+    '''tokenize at the word level'''
+    result = sent.strip().split()
+    i = 0
+    while i < len(result):
+        word = result[i]
+        j = k = 0
+        while word[j] in punctuation:
+            j += 1
+        while word[-1 - k] in punctuation:
+            k += 1
+        L = len(word)
+        if j + k < L:  # not all punctuation
+            result[i] = word[j:L - k]
+            if j > 0:
+                result.insert(i, word[:j])
+                i += 1
+                result.insert(i + 1, word[-k:])  # might want to do more to separate out '.' from '"', etc.
+                i += 1
+        i += 1
+    return result
 
 
-def main(accepted_file_types=['txt', 'tsv'], tsv_sentstart_mark='start'):
-
-    # setup
+def main():
+    # set up
     import os
     import nltk.data
+    import csv
     orig_wd = os.getcwd()
     toks = indices = None
-    args = _ap_parser(accepted_file_types).parse_args()
+    args = _ap_parser().parse_args()
     if args.log_level:
         logging.getLogger().setLevel(eval('logging.' + args.log_level.upper()))
     print('')
 
     # some file name processing
-    infile_parts = args.text.split('\\')[-1].split('.')
+    infile_parts = args.input_file.split('\\')[-1].split('.')
     infile_ext = infile_parts[-1].lower()
     infile_name = '.'.join(infile_parts[:-1])
+    tknzr_name = args.tokenizer.replace('\\', '/')
+    if not tknzr_name.endswith('.pickle'):
+        tknzr_name += '.pickle'
 
     # pre-process input file contents
     logging.info('Pre-processing input file...')
-    intext = open(args.text, encoding=args.encoding).read().strip()
-    if infile_ext == 'tsv':
-        corptext, toks, ncols = join_tokens(intext)
-    elif infile_ext == 'txt':
-        corptext = intext
+    lines = open(args.input_file, encoding=args.encoding).readlines()
+    token_column = args.token_column if args.token_column else 0
+    if args.word_tokenized or args.delimiter is not None or args.token_column is not None:
+        logging.info('Interpreting as tokens at word level.')
+        wt = True
+        corptext = [''] * len(lines)
+        for i, line in enumerate(lines):
+            lines[i] = line.strip().split(args.delimiter)
+            corptext[i] = lines[i][token_column]
     else:
-        raise RuntimeError('Only {} allowed.'.format(_disp_extns(accepted_file_types, 'and')))
+        logging.info('Interpreting as untokenized at word level (will tokenize).')
+        wt = False
+        corptext = '\n'.join(lines)
 
     # get tokenizer
     logging.info('Loading Punkt tokenizer...')
-    if args.tokenizer_folder:
-        os.chdir(args.tokenizer_folder)
-    tokenizer = nltk.data.load('{}.pickle'.format(args.language))
+    tokenizer = nltk.data.load(tknzr_name)
 
     # tokenize
     logging.warning('Note: Custom-trained sentence tokenizers are best for multilingual data.')
     logging.info('Tokenizing...')
-    sents = tokenizer.tokenize(corptext)
-    nsents = len(sents)
-
-    # get output contents
-    logging.info('Creating output file contents...')
-    if infile_ext == 'txt':
-        outtext = indices = ''
-        searchstart = 0
-        for i in range(nsents):
-            sent = sents[i]
-            outtext += sent + '\n'
-            index = searchstart + intext[searchstart:].find(sent)  # in case characters are lost b/w sentences after tokenizing (e.g. newline; others possible?)
-            indices += str(index) + ' '
-            searchstart = index + len(sent)
+    if wt:
+        sents = tokenizer.sentences_from_tokens(corptext)
     else:
-        rownum = 0
-        for i in range(nsents):
-            toks[rownum] += '\t' * (ncols - toks[rownum].count('\t')) + tsv_sentstart_mark  # mark start of sentence. NCOLS considered in case of inconsistent number of entries per row.
-            sent = sents[i]
-            sentlen = len(sent) - sent.count(' ')
-            logging.debug('sentlen: {}'.format(sentlen))
-            lencount = 0
-            while lencount < sentlen:
-                lencount += len(toks[rownum].split('\t')[0])
-                rownum += 1
-            logging.debug('lencount: {}'.format(lencount))
-            if lencount > sentlen:
-                logging.error('Sentence length mismatch at sentence #{}.'.format(i + 1))
-        outtext = '\n'.join(toks)
+        sents = tokenizer.sentences_from_text(corptext)
 
-    # write to output file(s)
+    # write to output file
     os.chdir(orig_wd)
     if args.output_folder:
         os.chdir(args.output_folder)
-    logging.info('Writing output file(s)...')
-    if infile_ext == 'txt':
-        lastpart = ' (sentence-tokenized).txt'
-    else:
-        lastpart = ' (marked for sentence starts).tsv'
+    lastpart = ' (marked for sentence ends).tsv'
     outfilename = infile_name + lastpart
-    outfile = open(outfilename, 'w', encoding=args.encoding)
-    outfile.write(outtext)
-    outfile.close()
-    if infile_ext == 'txt':
-        outfilename = infile_name + ' (sentence indices).txt'
-        outfile2 = open(outfilename, 'w', encoding=args.encoding)
-        outfile2.write(indices)
-        outfile2.close()
+    with open(outfilename, 'w', encoding=args.encoding) as outfile:
+        if args.encoding == 'utf8':  # other encoding types might have the same problem; haven't checked yet
+            csv.register_dialect('utf8-TSV', 'excel-tab', lineterminator='\n')
+            dialect = 'utf8-TSV'
+        else:
+            dialect = 'excel-tab'
+        writer = csv.writer(outfile, dialect)
+        if wt:
+            logging.info('Writing to output file...')
+            start = 0
+            for sent in sents:
+                L = len(sent)
+                for i, tok in enumerate(sent):
+                    line = lines[start + i]
+                    if line[token_column] != tok:
+                        logging.error("Token mismatch at line {}: '{}' != '{}'".format(start + i, tok, line[token_column]))
+                    if i == L - 1:
+                        line += ['SENT']
+                    else:
+                        line += ['']
+                    writer.writerow(line)
+                start += L
+        else:
+            logging.info('Word-tokenizing and writing to output file...')
+            for sent in sents:
+                toks = wordtok(sent)
+                L = len(toks)
+                for i, tok in enumerate(toks):
+                    if i == L - 1:
+                        line = [tok, 'SENT']
+                    else:
+                        line = [tok, '']
+                    writer.writerow(line)
 
     logging.info('Done.')
 
