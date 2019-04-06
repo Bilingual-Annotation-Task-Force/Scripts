b4947a2559858a9ed8419671898bb99e2d798e12
TongruiLi <tongrui1998@gmail.com>
2019-04-06 13:44:46 -0500

    updated flair pos tagger

--- pos.py
+++ pos.py
@@ -1,11 +1,23 @@
 import torch
 from flair.data import Sentence
-
 from flair.models import SequenceTagger
-tagger = SequenceTagger.load("pos-multi")
+import pickle
 
-with open("real_feature.txt") as f:
-    for l in f:
-        sentence = Sentence(l)
-        tagger.predict(sentence)
-        print(sentence.to_tagged_string(), file=open("KC_pos.txt", "a"))
+
+if __name__ == "__main__":
+    tagger = SequenceTagger.load("pos-multi")
+    with open("real_feature.txt", encoding = "utf-8-sig") as f:
+        sentences = []
+        # empty the contents
+        with open("KC_pos.txt", "w") as p, open("KC_pos_pickle", "w") as pp:
+            pass
+        with open("KC_pos.txt", "a", encoding = "utf-8-sig") as p:
+            for l in f:
+                sentence = Sentence(l)
+                tagger.predict(sentence)
+                sentences.append(sentence)
+                print(sentence.to_tagged_string(), file=p)
+            print("done outputing readable POS tag")
+        with open("KC_pos_pickle", "wb") as pp:
+            pickle.dump(sentences, pp)
+            print("done outputing serialized object")
